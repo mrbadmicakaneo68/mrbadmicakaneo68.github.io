266aae02d905f7c0dcbd6859791be7075759b132
mrbadmicakaneo68 <98886833+mrbadmicakaneo68@users.noreply.github.com>
2023-09-29 23:16:49 -0400

    update
    update 9/29

---
+++ test 2-1-2/Untitled-1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+// Declare an integer variable called myInt and assign the value 15 to it
+
+int myInt = 15;
+
+// Declare a pointer variable called myPointer using the dereference operator (*) and
+
+// assign it to the memory location of myInt using the address-of operator (&)
+
+int *myPointer = &myInt;
+
+// Print the memory location of myInt (remember the address-of operator!) and the value of myPointer.
+
+cout<<"Address of myInt : "<<&myInt<<endl;
+
+cout<<"value of myPointer : "<<myPointer<<endl<<endl;
+
+// Print the value of myInt and the value pointed to by myPointer (remember the dereference operator!)
+
+cout<<"Value of myInt : "<<myInt<<endl;
+
+cout<<"Value pointed by myPointer : "<<*myPointer<<endl<<endl;
+
+// Change the value of myInt to 10.
+
+myInt = 10;
+
+// Print the memory location of myInt (remember the address-of operator!) and the value of myPointer.
+
+cout<<"Address of myInt : "<<&myInt<<endl;
+
+cout<<"value of myPointer : "<<myPointer<<endl<<endl;
+
+// Print the value of myInt and the value pointed to by myPointer (remember the dereference operator!)
+
+cout<<"Value of myInt : "<<myInt<<endl;
+
+cout<<"Value pointed by myPointer : "<<*myPointer<<endl<<endl;
+
+return 0;
+
+}
